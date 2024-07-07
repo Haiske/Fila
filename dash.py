34c51d1066c8d24d98c2c8b34cc5a2ff7cd3972a
Futigami <96393652+Haiske@users.noreply.github.com>
2024-07-07 14:43:34 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -11,7 +11,7 @@ from time import sleep
 
 st.set_page_config('ESTOQUE • FILA', page_icon='https://i.imgur.com/TZp66zI.png', layout='wide')
     
-st.image('https://i.imgur.com/QgNqMAu.png', width=200)
+st.image('https://i.imgur.com/QgNqMAu.png', width=400)
 st.header('', divider='gray')
 
 st.sidebar.title('MÓDULOS')
