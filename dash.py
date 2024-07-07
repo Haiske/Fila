0ae9d6d053ee332dec3b70870a5a025651e885ea
Futigami <96393652+Haiske@users.noreply.github.com>
2024-07-07 18:39:43 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -27,8 +27,12 @@ def create_df_historico_movimentações():
     
     historico_fila['ULTIMA DATA'] = historico_fila['DT ENVIO LAB']
     historico_fila.loc[historico_fila['ULTIMA DATA'].isna(), 'ULTIMA DATA'] = date(2024, 7, 1)
+    
     historico_fila['ULTIMA DATA'] = pd.to_datetime(historico_fila['ULTIMA DATA'])
     historico_fila['DT RECEBIMENTO'] = pd.to_datetime(historico_fila['DT RECEBIMENTO'])
+    historico_fila.loc[~historico_fila['DT RECEBIMENTO'].isna(), 'DT RECEBIMENTO'] = pd.to_datetime(historico_fila.loc[~historico_fila['DT RECEBIMENTO'].isna(), 'DT RECEBIMENTO'])
+
+    
     historico_fila['AGING TOTAL'] = historico_fila.apply(lambda row: calendario.get_working_days_delta(row['DT RECEBIMENTO'], row['ULTIMA DATA']), axis=1) + 1
     historico_fila['AGING TOTAL'] = historico_fila['AGING TOTAL'].astype('int')
     
