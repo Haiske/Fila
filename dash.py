1b8bce2f62c9300f1e0e8fcad261a1bbce2fdfe4
Futigami <96393652+Haiske@users.noreply.github.com>
2024-07-07 14:40:31 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -9,27 +9,14 @@ import plotly.express as px
 from workalendar.america import Brazil
 from time import sleep
 
-if 'connection' not in st.session_state:
-    with st.form('login', clear_on_submit=True):
-        usuario = st.text_input(label='Usuário')
-        senha = st.text_input(label='Senha', type='password')
-        submitted = st.form_submit_button("Login")
-        if submitted:
-            with st.spinner('Conectando...'):
-                if usuario == st.secrets.credenciais.USER and senha == st.secrets.credenciais.SENHA:
-                    st.session_state['connection'] = 'editor'
-                    st.rerun()
-                else:
-                    st.warning('Usuário ou senha inválidos.', icon="⚠️")
-else:
-    st.set_page_config('ESTOQUE • FILA', page_icon='https://i.imgur.com/mOEfCM8.png', layout='wide')
-        
-    st.image('https://seeklogo.com/images/G/gertec-logo-D1C911377C-seeklogo.com.png?v=637843433630000000', width=200)
-    st.header('', divider='gray')
+st.set_page_config('ESTOQUE • FILA', page_icon='https://imgur.com/a/mo1ofMG', layout='wide')
+    
+st.image('https://seeklogo.com/images/G/gertec-logo-D1C911377C-seeklogo.com.png?v=637843433630000000', width=200)
+st.header('', divider='gray')
 
-    st.sidebar.title('MÓDULOS')
-    if st.session_state['connection'] == 'editor':
-        st.sidebar.page_link('pages/4_Movimentações.py', label='MOVIMENTAÇÕES')
-    st.sidebar.page_link('pages/1_Contratos.py', label='CONTRATO')
-    st.sidebar.page_link('pages/2_Varejo.py', label='VAREJO')
-    st.sidebar.page_link('pages/3_OS Interna.py', label='OS INTERNA')
+st.sidebar.title('MÓDULOS')
+if st.session_state['connection'] == 'editor':
+    st.sidebar.page_link('pages/4_Movimentações.py', label='MOVIMENTAÇÕES')
+st.sidebar.page_link('pages/1_Contratos.py', label='CONTRATO')
+st.sidebar.page_link('pages/2_Varejo.py', label='VAREJO')
+st.sidebar.page_link('pages/3_OS Interna.py', label='OS INTERNA')
