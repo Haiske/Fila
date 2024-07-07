bcf2fc171f664aa3837934b1067bfeb2a68e1c4b
Futigami <96393652+Haiske@users.noreply.github.com>
2024-07-07 17:53:02 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd
-from office365.sharepoint.files.file import File
-from office365.sharepoint.client_context import ClientContext
-from office365.runtime.auth.authentication_context import AuthenticationContext
 from datetime import datetime
 import io
 import plotly.express as px
@@ -10,10 +7,8 @@ from workalendar.america import Brazil
 from time import sleep
 
 st.set_page_config('ESTOQUE • FILA', page_icon='https://raw.githubusercontent.com/Haiske/Fila/main/attachments/icon.png', layout='wide')
-
 st.logo('https://raw.githubusercontent.com/Haiske/Fila/main/attachments/logo.png', icon_image='https://raw.githubusercontent.com/Haiske/Fila/main/attachments/icon.png')
-
-st.image('https://raw.githubusercontent.com/Haiske/Fila/main/attachments/logo.png', width=400)
+st.image('https://raw.githubusercontent.com/Haiske/Fila/main/attachments/logo.png', width=200)
 st.divider()
 
 st.sidebar.title('MÓDULOS')
